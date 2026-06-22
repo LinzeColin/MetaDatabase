c15d51465ef711336f9788c063a8710d176222b2
LinzeZhang <68840188+LinzeColin@users.noreply.github.com>
2026-06-22 11:29:24 +1000

    Use subcategory queries for arXiv archives

--- arxiv-daily-push/src/arxiv_daily_push/global_scan.py
+++ arxiv-daily-push/src/arxiv_daily_push/global_scan.py
@@ -30,12 +30,20 @@ ROI_SELECTION_MIN_SCORE = 50.0
 ROI_QUEUE_MIN_SCORE = 55.0
 
 ALL_ARXIV_ARCHIVES: tuple[dict[str, str], ...] = (
-    {"archive_id": "cs", "group": "Computer Science", "query": "cat:cs"},
-    {"archive_id": "econ", "group": "Economics", "query": "cat:econ"},
-    {"archive_id": "eess", "group": "Electrical Engineering and Systems Science", "query": "cat:eess"},
-    {"archive_id": "math", "group": "Mathematics", "query": "cat:math"},
-    {"archive_id": "astro-ph", "group": "Physics", "query": "cat:astro-ph"},
-    {"archive_id": "cond-mat", "group": "Physics", "query": "cat:cond-mat"},
+    {
+        "archive_id": "cs",
+        "group": "Computer Science",
+        "query": "cat:cs.AI OR cat:cs.AR OR cat:cs.CC OR cat:cs.CE OR cat:cs.CG OR cat:cs.CL OR cat:cs.CR OR cat:cs.CV OR cat:cs.CY OR cat:cs.DB OR cat:cs.DC OR cat:cs.DL OR cat:cs.DM OR cat:cs.DS OR cat:cs.ET OR cat:cs.FL OR cat:cs.GL OR cat:cs.GR OR cat:cs.GT OR cat:cs.HC OR cat:cs.IR OR cat:cs.IT OR cat:cs.LG OR cat:cs.LO OR cat:cs.MA OR cat:cs.MM OR cat:cs.MS OR cat:cs.NA OR cat:cs.NE OR cat:cs.NI OR cat:cs.OH OR cat:cs.OS OR cat:cs.PF OR cat:cs.PL OR cat:cs.RO OR cat:cs.SC OR cat:cs.SD OR cat:cs.SE OR cat:cs.SI OR cat:cs.SY",
+    },
+    {"archive_id": "econ", "group": "Economics", "query": "cat:econ.EM OR cat:econ.GN OR cat:econ.TH"},
+    {"archive_id": "eess", "group": "Electrical Engineering and Systems Science", "query": "cat:eess.AS OR cat:eess.IV OR cat:eess.SP OR cat:eess.SY"},
+    {
+        "archive_id": "math",
+        "group": "Mathematics",
+        "query": "cat:math.AC OR cat:math.AG OR cat:math.AP OR cat:math.AT OR cat:math.CA OR cat:math.CO OR cat:math.CT OR cat:math.CV OR cat:math.DG OR cat:math.DS OR cat:math.FA OR cat:math.GM OR cat:math.GN OR cat:math.GR OR cat:math.GT OR cat:math.HO OR cat:math.IT OR cat:math.KT OR cat:math.LO OR cat:math.MG OR cat:math.MP OR cat:math.NA OR cat:math.NT OR cat:math.OA OR cat:math.OC OR cat:math.PR OR cat:math.QA OR cat:math.RA OR cat:math.RT OR cat:math.SG OR cat:math.SP OR cat:math.ST",
+    },
+    {"archive_id": "astro-ph", "group": "Physics", "query": "cat:astro-ph.CO OR cat:astro-ph.EP OR cat:astro-ph.GA OR cat:astro-ph.HE OR cat:astro-ph.IM OR cat:astro-ph.SR"},
+    {"archive_id": "cond-mat", "group": "Physics", "query": "cat:cond-mat.dis-nn OR cat:cond-mat.mes-hall OR cat:cond-mat.mtrl-sci OR cat:cond-mat.other OR cat:cond-mat.quant-gas OR cat:cond-mat.soft OR cat:cond-mat.stat-mech OR cat:cond-mat.str-el OR cat:cond-mat.supr-con"},
     {"archive_id": "gr-qc", "group": "Physics", "query": "cat:gr-qc"},
     {"archive_id": "hep-ex", "group": "Physics", "query": "cat:hep-ex"},
     {"archive_id": "hep-lat", "group": "Physics", "query": "cat:hep-lat"},
@@ -51,7 +59,7 @@ ALL_ARXIV_ARCHIVES: tuple[dict[str, str], ...] = (
         "query": "cat:physics.acc-ph OR cat:physics.app-ph OR cat:physics.atm-clus OR cat:physics.atom-ph OR cat:physics.bio-ph OR cat:physics.chem-ph OR cat:physics.class-ph OR cat:physics.comp-ph OR cat:physics.data-an OR cat:physics.flu-dyn OR cat:physics.gen-ph OR cat:physics.geo-ph OR cat:physics.hist-ph OR cat:physics.ins-det OR cat:physics.med-ph OR cat:physics.optics OR cat:physics.plasm-ph OR cat:physics.pop-ph OR cat:physics.soc-ph OR cat:physics.space-ph",
     },
     {"archive_id": "quant-ph", "group": "Physics", "query": "cat:quant-ph"},
-    {"archive_id": "q-bio", "group": "Quantitative Biology", "query": "cat:q-bio"},
+    {"archive_id": "q-bio", "group": "Quantitative Biology", "query": "cat:q-bio.BM OR cat:q-bio.CB OR cat:q-bio.GN OR cat:q-bio.MN OR cat:q-bio.NC OR cat:q-bio.OT OR cat:q-bio.PE OR cat:q-bio.QM OR cat:q-bio.SC OR cat:q-bio.TO"},
     {"archive_id": "q-fin", "group": "Quantitative Finance", "query": "cat:q-fin.CP OR cat:q-fin.EC OR cat:q-fin.GN OR cat:q-fin.MF OR cat:q-fin.PM OR cat:q-fin.PR OR cat:q-fin.RM OR cat:q-fin.ST OR cat:q-fin.TR"},
     {"archive_id": "stat", "group": "Statistics", "query": "cat:stat.AP OR cat:stat.CO OR cat:stat.ME OR cat:stat.ML OR cat:stat.OT OR cat:stat.TH"},
 )
